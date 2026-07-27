3ed91b404b4323e203ca1290414cfd1d3c052e65
unknown <chett.tiller@gmail.com>
2026-07-27 15:09:41 -0400

    day one afternoon

--- 01/c-functions.js
+++ 01/c-functions.js
@@ -54,3 +54,62 @@ function consoleLogStuff() {
 }
 
 // ARROW FN //
+
+                            // with one line this has implicit return
+const myArrowFunction = () => "I AM AN ARROW FN"
+
+// ANONYMOUS FN //
+
+// anonymous functions are fns without names
+// function() {}
+// () => {}
+
+// ---EXERCISES UNTIL 2:43 EST--- //
+
+// 1. Create a fn fullName() 
+// - which accepts two arguments: firstName and lastName
+// - returns the two names in this format: "firstName lastName"
+
+// Example: fullName("Chett", "Tiller") ==> "Chett Tiller"
+
+function fullName(firstName, lastName) {
+    return `${firstName} ${lastName}`
+}
+
+
+// 2. Create a fn getRemainder()
+// - accepts two arguments: num and divisor
+// - use the modulo (%) to get the remainder of num and the divisor
+// - return result
+
+// Example: getRemainder(5,3) ==> 2
+
+const getRemainder = (num, divisor) => {
+    return num % divisor
+}
+
+const shortRemainder = (num, divisor) => num % divisor
+
+
+// 3. Create a fn displayHours()
+// - accepts two arguments: opening and closing
+// - returns a string formatted like this: "We are open from opening until closing."
+
+// Example: displayHours("7am", "5pm") ==> "We are open from 7am until 5pm"
+// `We are open from ${timeOne} until ${timeTwo}`
+
+function displayHours(open, close) {
+    // return "We are open from " + open + " until " + close
+    return `We are open from ${open} until ${close}`
+}
+
+// RETURNS //
+
+// when you return, the fn ends
+function multipleReturns(open, close) {
+    return `We are open from ${open} until ${close}`
+    // everything below is dead code in the fn
+    return `I would never open between ${open} and ${close}`
+    return `OPEN: ${open} CLOSE: ${close}`
+    return "I AM THE FOURTH RETURN STATEMENT"
+}
