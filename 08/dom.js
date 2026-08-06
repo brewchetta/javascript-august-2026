cb2bd3f695bd45472c00edc2f9fbe87bddcf267e
Chett Tiller <chett.tiller@gmail.com>
2026-08-06 12:52:24 -0400

    dom exercises

--- 08/dom.js
+++ 08/dom.js
@@ -26,3 +26,33 @@ animalList.children[2]
 animalList.querySelectorAll('li')[2]
 document.querySelector("#animal-list").children[2]
 document.querySelector("#animal-list").querySelectorAll('li')[2]
+
+// <!-- 1. Can you console.log the text inside the h2 in the second div -->
+
+const secondDiv = document.querySelectorAll('div')[1]
+secondDiv.querySelector('h2')
+// OR
+document.querySelectorAll('div')[1].querySelector('h2') 
+// OR
+document.querySelectorAll('div')[1].children[0]
+
+// <!-- 2. Can you console.log the text for the first li inside the second div -->
+
+document.querySelectorAll('div')[1].children[3].children[0].textContent 
+
+// <!-- 3. Can you console.log the span text inside the inner li -->
+
+document.querySelectorAll('div')[1].querySelector('ul').children[3].children[0]
+
+// OR
+
+document.querySelector('span') 
+// this only works because we only have a single span on the page
+
+// <!-- 4. Can you console.log the value inside the textarea (what a user has typed) -->
+
+document.querySelector('textarea').value
+
+document.querySelector('#likes-about-js').value
+
+document.querySelectorAll('div')[1].querySelector('form').querySelector('textarea').value
