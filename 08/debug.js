c19d2415ab600370b7250859b9ef4793d63857e8
brewchetta <chett.tiller@gmail.com>
2026-08-06 17:01:34 -0400

    debugging challenge completed

--- 08/debug.js
+++ 08/debug.js
@@ -10,37 +10,75 @@
 
 // BUG ONE:
 // result: set width of image to 50%
-querySelector( 'img' ).width = "50%"
+// querySelector( 'img' ).width = "50%"
+
+// SOLUTION:
+document.querySelector('img').style.width = "30%"
+
 
 // BUG TWO:
 // result: increase number of defeated bugs when "Defeat Bug" button is clicked
-const counter = 0
+// const counter = 0
 
-function increseCounter( defeatedBugsText ) {
+// function increseCounter( defeatedBugsText ) {
+//     counter += 1
+//     defeatedBugsText.textContent = counter
+// }
+
+// defeatBugButton.addEventListener( "click", increaseCounter )
+
+// const defeatBugButton = querySelector( 'defeat-bug-button' )
+
+// SOLUTION: 
+let counter = 0
+
+function increaseCounter( unusedEvent ) {
     counter += 1
-    defeatedBugsText.textContent = counter
+    const defeatedBugsSpan = document.querySelector('span')
+    defeatedBugsSpan.textContent = counter
 }
+
+const defeatBugButton = document.querySelector( '#defeat-bug-button' )
 
 defeatBugButton.addEventListener( "click", increaseCounter )
 
-const defeatBugButton = querySelector( 'defeat-bug-button' )
 
 // BUG THREE:
 // result: change list items to a color such as "yellow"
+// function changeListItemsToColor( color ) {
+//     const listItems = querySelector( 'li' )
+//     for ( let i = 0; i < listItems; i++ ) {
+//         changeListItemColor( listItems )
+//     }
+// }
+
+// function changeListItemColor( listItems ) {
+//     listItems.color = color
+// }
+
+// changeListItemsToColor
+
+// SOLUTION: 
+changeListItemsToColor("magenta")
+
 function changeListItemsToColor( color ) {
-    const listItems = querySelector( 'li' )
-    for ( let i = 0; i < listItems; i++ ) {
-        changeListItemColor( listItems )
+    const listItems = document.querySelectorAll( 'li' )
+
+    for ( let i = 0; i < listItems.length; i++ ) {
+        changeListItemColor( listItems[i], color )
     }
 }
 
-function changeListItemColor( listItems ) {
-    listItems.color = color
+function changeListItemColor( listItem, color ) {
+    listItem.style.color = color
 }
 
-changeListItemsToColor
 
 // BUG FOUR:
 // result: change body background and text color
-body.style.backgroundColor = black
-body.style.color = white
+// body.style.backgroundColor = black
+// body.style.color = white
+
+// SOLUTION:
+document.body.style.backgroundColor = "black"
+document.body.style.color = "white"
