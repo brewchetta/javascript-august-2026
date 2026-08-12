03c227bf53b6070ce808f7ee34c4eb22e29be995
brewchetta <chett.tiller@gmail.com>
2026-08-12 14:59:55 -0400

    movie exercises complete

--- 11/02-movies.js
+++ 11/02-movies.js
@@ -13,9 +13,14 @@ const movies = [
 ]
 
 // Use .sort() to reorder the array alphabetically
+movies.sort((a,b) => a > b)
 
 // Use .filter() to return an array of movies with multiple words (HINT: they'll include a space)
+movies.filter(movieTitle => movieTitle.includes(' ')) 
 
 // Use .slice() to return a new array of only the first 5 movies
+movies.slice(0, 5)
 
 // Use .join() to return a string representing all the movies
+movies.join()
+movies.join(" | ") 
